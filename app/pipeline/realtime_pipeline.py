c9f41fc5a8af28b647c55d4feb9fb44d8c67644b
RealAlvariTarfa <alvaritarfa@gmail.com>
2026-03-13 07:20:57 +0000

    Update STT pipeline

--- app/pipeline/realtime_pipeline.py
+++ app/pipeline/realtime_pipeline.py
@@ -1,27 +1,23 @@
 import asyncio
 
-from app.audio.capture import capture_audio
-from app.audio.playback import playback_audio
-from app.stt.stt_stream import streaming_stt
+
+async def test1():
+    print("Task 1 started")
+    while True:
+        await asyncio.sleep(1)
+
+
+async def test2():
+    print("Task 2 started")
+    while True:
+        await asyncio.sleep(1)
 
 
 async def main():
 
     print("Audio pipeline started...")
 
-    audio_queue = asyncio.Queue(maxsize=50)
-    text_queue = asyncio.Queue()
-
-    loop = asyncio.get_running_loop()
-
     await asyncio.gather(
-
-        # run capture in background thread
-        loop.run_in_executor(None, capture_audio, audio_queue),
-
-        # STT task
-        streaming_stt(audio_queue, text_queue),
-
-        # playback task
-        playback_audio(audio_queue),
+        test1(),
+        test2(),
     )
